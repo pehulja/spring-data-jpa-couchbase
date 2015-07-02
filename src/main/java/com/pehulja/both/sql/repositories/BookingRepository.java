c16ec8c1c87226885efeeb778ac4c1dbf571a460
Eugene Pehulja <pehulja@gmail.com>
2015-07-02 17:37:06 +0300

    Before branching

--- src/main/java/com/pehulja/both/sql/repositories/BookingRepository.java
+++ src/main/java/com/pehulja/both/sql/repositories/BookingRepository.java
@@ -1,18 +1,17 @@
 package com.pehulja.both.sql.repositories;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.CrudRepository;
 
 import com.pehulja.both.sql.model.Booking;
-import com.pehulja.both.sql.model.Guest;
+import com.pehulja.both.sql.repositories.custom.BookingRepositoryCustom;
 
 @Profile("jpa")
-public interface BookingRepository extends JpaRepository<Booking, Integer>, BookingRepositoryCustom{
+public interface BookingRepository extends CrudRepository<Booking, Integer>, BookingRepositoryCustom {
 
-	/*@Query("select booking from Booking booking where booking.guest = :guest")
-	public List<Booking> getBookingsByGuest(@Param("guest") Guest guest);*/
+	/*
+	 * @Query("select booking from Booking booking where booking.guest = :guest"
+	 * ) public List<Booking> getBookingsByGuest(@Param("guest") Guest guest);
+	 */
 }
--- src/main/java/com/pehulja/both/sql/repositories/GuestRepository.java
+++ src/main/java/com/pehulja/both/sql/repositories/GuestRepository.java
@@ -2,10 +2,11 @@ package com.pehulja.both.sql.repositories;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.pehulja.both.sql.model.Guest;
 
 @Profile("jpa")
-public interface GuestRepository extends JpaRepository<Guest, Integer> {
+public interface GuestRepository extends CrudRepository<Guest, Integer> {
 
 }
