3b2f5ec72336c3b4d4611b636e22250f601ea009
Eugene Pehulja <pehulja@gmail.com>
2015-07-02 17:39:10 +0300

    lala

--- src/main/java/com/pehulja/both/nosql/repositories/GuestRepository.java
+++ src/main/java/com/pehulja/both/nosql/repositories/GuestRepository.java
@@ -3,7 +3,9 @@ package com.pehulja.both.nosql.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.pehulja.both.nosql.model.Guest;
-
+/*
+ * lalalal
+ */
 public interface GuestRepository extends CrudRepository<Guest, String> {
 
 }
